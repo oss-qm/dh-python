cba856ffc91b77737cc08bb1e98152adb2551a6e
Enrico Weigelt, metux IT consult <info@metux.net>
2018-08-14 21:03:00 +0200

    debian tarball dh-python_2.20170125_bpo8+1.tar.xz

---
+++ pydist/generate_fallback_list.py
@@ -0,0 +1,141 @@
+#! /usr/bin/python3
+# Copyright © 2010-2015 Piotr Ożarowski <piotr@debian.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import re
+import sys
+try:
+    from distro_info import DistroInfo  # python3-distro-info package
+except ImportError:
+    DistroInfo = None
+from gzip import decompress
+from os import chdir, mkdir
+from os.path import dirname, exists, isdir, join, split
+from urllib.request import urlopen
+
+if '--ubuntu' in sys.argv and DistroInfo:
+    SOURCE = 'http://archive.ubuntu.com/ubuntu/dists/%s/Contents-amd64.gz' % \
+             DistroInfo('ubuntu').devel()
+else:
+    SOURCE = 'http://ftp.debian.org/debian/dists/jessie/main/Contents-amd64.gz'
+
+IGNORED_PKGS = {'python-setuptools', 'python3-setuptools', 'pypy-setuptools'}
+DEFAULTS = {
+    'cpython2': [
+        'python python\n',
+        'setuptools python-pkg-resources\n',
+        'wsgiref python (>= 2.5) | python-wsgiref\n',
+        'argparse python (>= 2.7) | python-argparse\n',
+        # not recognized due to .pth file (egg-info is in PIL/ and not in *-packages/)
+        'pil python-pil\n',
+        'Pillow python-pil\n'],
+    'cpython3': [
+        'pil python3-pil\n',
+        'Pillow python3-pil\n',
+        'setuptools python3-pkg-resources\n',
+        'argparse python3 (>= 3.2)\n'],
+    'pypy': []
+}
+
+public_egg = re.compile(r'''
+    /usr/
+    (
+        (?P<cpython2>
+            (lib/python2\.[0-9]/((site)|(dist))-packages)|
+            (share/python-support/[^/]+)
+        )|
+        (?P<cpython3>
+            (lib/python3/dist-packages)
+        )|
+        (?P<pypy>
+            (lib/pypy/dist-packages)
+        )
+    )
+    /[^/]*\.egg-info
+''', re.VERBOSE).match
+
+skip_sensible_names = True if '--skip-sensible-names' in sys.argv else False
+
+chdir(dirname(__file__))
+if isdir('../dhpython'):
+    sys.path.append('..')
+else:
+    sys.path.append('/usr/share/dh-python/dhpython/')
+from dhpython.pydist import sensible_pname
+
+if not isdir('cache'):
+    mkdir('cache')
+cache_fpath = join('cache', split(SOURCE)[-1])
+if not exists(cache_fpath):
+    data = urlopen(SOURCE).read()
+    with open(cache_fpath, 'wb') as fp:
+        fp.write(data)
+else:
+    data = open(cache_fpath, 'rb').read()
+try:
+    data = str(decompress(data), encoding='UTF-8')
+except UnicodeDecodeError as e:  # Ubuntu
+    data = str(decompress(data), encoding='ISO-8859-15')
+
+result = {
+    'cpython2': {},
+    'cpython3': {},
+    'pypy': {}}
+
+# Contents file doesn't contain comment these days
+is_header = not data.startswith('bin')
+for line in data.splitlines():
+    if is_header:
+        if line.startswith('FILE'):
+            is_header = False
+        continue
+    try:
+        path, desc = line.rsplit(maxsplit=1)
+    except ValueError:
+        # NOTE(jamespage) some lines in Ubuntu are not parseable.
+        continue
+    path = '/' + path.rstrip()
+    section, pkg_name = desc.rsplit('/', 1)
+    if pkg_name in IGNORED_PKGS:
+        continue
+    match = public_egg(path)
+    if match:
+        egg_name = [i.split('-', 1)[0] for i in path.split('/')
+                    if i.endswith('.egg-info')][0]
+        if egg_name.endswith('.egg'):
+            egg_name = egg_name[:-4]
+
+        impl = next(key for key, value in match.groupdict().items() if value)
+
+        if skip_sensible_names and\
+                sensible_pname(impl, egg_name) == pkg_name:
+            continue
+
+        processed = result[impl]
+        if egg_name not in processed:
+            processed[egg_name] = pkg_name
+
+for impl, details in result.items():
+    with open('{}_fallback'.format(impl), 'w') as fp:
+        result = DEFAULTS[impl]
+        if result:
+            fp.writelines(result)
+        result = sorted('{} {}\n'.format(egg, pkg) for egg, pkg in details.items())
+        fp.writelines(result)
---
+++ tests/test_interpreter.py
@@ -0,0 +1,165 @@
+import unittest
+from os import environ
+from os.path import exists
+from dhpython.interpreter import Interpreter
+
+
+class TestInterpreter(unittest.TestCase):
+    def setUp(self):
+        self._triplet = environ.get('DEB_HOST_MULTIARCH')
+        environ['DEB_HOST_MULTIARCH'] = 'MYARCH'
+
+    def tearDown(self):
+        if self._triplet:
+            environ['DEB_HOST_MULTIARCH'] = self._triplet
+
+    @unittest.skipUnless(exists('/usr/bin/pypy'), 'pypy is not installed')
+    def test_pypy(self):
+        sorted(Interpreter.parse('pypy').items()) == \
+            [('debug', None), ('name', 'pypy'), ('options', ()), ('path', ''), ('version', None)]
+        sorted(Interpreter.parse('#! /usr/bin/pypy --foo').items()) == \
+            [('debug', None), ('name', 'pypy'), ('options', ('--foo',)), ('path', '/usr/bin/'), ('version', None)]
+        Interpreter('pypy').sitedir(version='2.0') == '/usr/lib/pypy/dist-packages/'
+
+    @unittest.skipUnless(exists('/usr/bin/python2.6'), 'python2.6 is not installed')
+    def test_python26(self):
+        i = Interpreter('python2.6')
+        self.assertEqual(i.soabi(), '')
+        self.assertIsNone(i.check_extname('foo.so'))
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertIsNone(i.check_extname('foo/bar/bazmodule.so'))
+
+    @unittest.skipUnless(exists('/usr/bin/python2.6-dbg'), 'python2.6-dbg is not installed')
+    def test_python26dbg(self):
+        i = Interpreter('python2.6-dbg')
+        self.assertEqual(i.soabi(), '')
+        self.assertIsNone(i.check_extname('foo_d.so'))
+        self.assertEqual(i.check_extname('foo.so'), 'foo_d.so')
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), 'foo/bar/bazmodule_d.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python2.7'), 'python2.7 is not installed')
+    def test_python27(self):
+        i = Interpreter('python2.7')
+        self.assertEqual(i.soabi(), '')
+        self.assertEqual(i.check_extname('foo.so'), 'foo.MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.MYARCH_d.so'))
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertIsNone(i.check_extname('foo.OTHER.so'))  # different architecture
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), 'foo/bar/baz.MYARCH.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python2.7-dbg'), 'python2.7-dbg is not installed')
+    def test_python27dbg(self):
+        i = Interpreter('python2.7-dbg')
+        self.assertEqual(i.soabi(), '')
+        self.assertEqual(i.check_extname('foo.so'), 'foo.MYARCH_d.so')
+        self.assertEqual(i.check_extname('foo_d.so'), 'foo.MYARCH_d.so')
+        self.assertIsNone(i.check_extname('foo.MYARCH_d.so'))
+        self.assertIsNone(i.check_extname('foo.OTHER_d.so'))  # different architecture
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), 'foo/bar/baz.MYARCH_d.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python3.1'), 'python3.1 is not installed')
+    def test_python31(self):
+        i = Interpreter('python3.1')
+        self.assertEqual(i.soabi(), '')
+        self.assertIsNone(i.check_extname('foo.so'))
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertIsNone(i.check_extname('foo/bar/bazmodule.so'))
+
+    @unittest.skipUnless(exists('/usr/bin/python3.1-dbg'), 'python3.1-dbg is not installed')
+    def test_python31dbg(self):
+        i = Interpreter('python3.1-dbg')
+        self.assertEqual(i.soabi(), '')
+        self.assertIsNone(i.check_extname('foo.so'))
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertIsNone(i.check_extname('foo/bar/bazmodule.so'))
+
+    @unittest.skipUnless(exists('/usr/bin/python3.2'), 'python3.2 is not installed')
+    def test_python32(self):
+        i = Interpreter('python3.2')
+        self.assertEqual(i.soabi(), 'cpython-32mu')
+        self.assertEqual(i.check_extname('foo.so'), r'foo.cpython-32mu.so')
+        self.assertIsNone(i.check_extname('foo.cpython-33m.so'))  # different version
+        self.assertIsNone(i.check_extname('foo.cpython-32mu-OTHER.so'))  # different architecture
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), r'foo/bar/bazmodule.cpython-32mu.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python3.2-dbg'), 'python3.2-dbg is not installed')
+    def test_python32dbg(self):
+        i = Interpreter('python3.2-dbg')
+        self.assertEqual(i.soabi(), 'cpython-32dmu')
+        self.assertEqual(i.check_extname('foo.so'), r'foo.cpython-32dmu.so')
+        self.assertIsNone(i.check_extname('foo.cpython-33m.so'))  # different version
+        self.assertIsNone(i.check_extname('foo.cpython-32dmu-OTHER.so'))  # different architecture
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), r'foo/bar/bazmodule.cpython-32dmu.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python3.4'), 'python3.4 is not installed')
+    def test_python34(self):
+        i = Interpreter('python3.4')
+        self.assertEqual(i.soabi(), 'cpython-34m')
+        self.assertEqual(i.check_extname('foo.so'), r'foo.cpython-34m-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.cpython-32m.so'))  # different version
+        self.assertIsNone(i.check_extname('foo.cpython-34m-OTHER.so'))  # different architecture
+        self.assertEqual(i.check_extname('foo.cpython-34m.so'), r'foo.cpython-34m-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), r'foo/bar/baz.cpython-34m-MYARCH.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python3.4-dbg'), 'python3.4-dbg is not installed')
+    def test_python34dbg(self):
+        i = Interpreter('python3.4-dbg')
+        self.assertEqual(i.soabi(), 'cpython-34dm')
+        self.assertEqual(i.check_extname('foo.so'), r'foo.cpython-34dm-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.cpython-32m.so'))  # different version
+        self.assertIsNone(i.check_extname('foo.cpython-34m-OTHER.so'))  # different architecture
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), r'foo/bar/baz.cpython-34dm-MYARCH.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python3.5'), 'python3.5 is not installed')
+    def test_python35(self):
+        i = Interpreter('python3.5')
+        self.assertEqual(i.soabi(), 'cpython-35m')
+        self.assertEqual(i.check_extname('foo.so'), r'foo.cpython-35m-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.cpython-32m.so'))  # different version
+        self.assertIsNone(i.check_extname('foo.cpython-35m-OTHER.so'))  # different architecture
+        self.assertEqual(i.check_extname('foo.cpython-35m.so'), r'foo.cpython-35m-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), r'foo/bar/baz.cpython-35m-MYARCH.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python3.5-dbg'), 'python3.5-dbg is not installed')
+    def test_python35dbg(self):
+        i = Interpreter('python3.5-dbg')
+        self.assertEqual(i.soabi(), 'cpython-35dm')
+        self.assertEqual(i.check_extname('foo.so'), r'foo.cpython-35dm-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.cpython-32m.so'))  # different version
+        self.assertIsNone(i.check_extname('foo.cpython-35m-OTHER.so'))  # different architecture
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), r'foo/bar/baz.cpython-35dm-MYARCH.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python3.6'), 'python3.6 is not installed')
+    def test_python36(self):
+        i = Interpreter('python3.6')
+        self.assertEqual(i.soabi(), 'cpython-36m')
+        self.assertEqual(i.check_extname('foo.so'), r'foo.cpython-36m-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.cpython-32m.so'))  # different version
+        self.assertIsNone(i.check_extname('foo.cpython-36m-OTHER.so'))  # different architecture
+        self.assertEqual(i.check_extname('foo.cpython-36m.so'), r'foo.cpython-36m-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), r'foo/bar/baz.cpython-36m-MYARCH.so')
+
+    @unittest.skipUnless(exists('/usr/bin/python3.6-dbg'), 'python3.6-dbg is not installed')
+    def test_python36dbg(self):
+        i = Interpreter('python3.6-dbg')
+        self.assertEqual(i.soabi(), 'cpython-36dm')
+        self.assertEqual(i.check_extname('foo.so'), r'foo.cpython-36dm-MYARCH.so')
+        self.assertIsNone(i.check_extname('foo.cpython-32m.so'))  # different version
+        self.assertIsNone(i.check_extname('foo.cpython-36m-OTHER.so'))  # different architecture
+        self.assertIsNone(i.check_extname('foo.abi3.so'))
+        self.assertEqual(i.check_extname('foo/bar/bazmodule.so'), r'foo/bar/baz.cpython-36dm-MYARCH.so')
+
+    def test_version(self):
+        i = Interpreter(impl='cpython2')
+        self.assertEqual(str(i), 'python')
+        self.assertEqual(i.binary('2.7'), '/usr/bin/python2.7')
+
+if __name__ == '__main__':
+    unittest.main()
